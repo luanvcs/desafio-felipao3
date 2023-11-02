4480721843aa67ea271577f7c64a3e1a357e0a1b
luanvcs <luan.v.c.s13@gmail.com>
2023-11-02 19:10:48 -0300

    projeto criado 3

---
+++ index.js
@@ -0,0 +1,30 @@
+class Hero {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque = "";
+      switch (this.tipo) {
+        case "mago":
+          ataque = "magia";
+          break;
+        case "guerreiro":
+          ataque = "espada";
+          break;
+        case "monge":
+          ataque = "artes marciais";
+          break;
+        case "ninja":
+          ataque = "shuriken";
+          break;
+      }
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  const meuGuerreiro = new Hero("Herói", 30, "guerreiro");
+  meuGuerreiro.atacar();
+  
